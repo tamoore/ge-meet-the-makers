c4c4d78b9a939910069e3686e664961867322072
James Robinson <james@imaginary.com.au>
2015-04-27 13:51:27 +1000

    Updates to the markup Basic time functions to: - populate the local clock - set offset based on time - sets hands on clock face

--- src/lib/index.js
+++ src/lib/index.js
@@ -6,6 +6,51 @@ class Application {
 	}
 }
 
+function localTime() {
+	var now = new Date();
+	var hours = now.getHours();
+	var	minutes = now.getMinutes();
+	var clock = document.getElementById('localTime');
+
+	// Create offset measurement for scroller
+	// Entire scroller = 96 blocks of 15 minutes (add another two units for spacer between 00 and 24)
+	
+	var offsetHours = parseInt(hours) * 4;
+	var offsetMinutes = Math.round( minutes / 15 );
+	clock.setAttribute('data-start', offsetHours + offsetMinutes);
+
+	// Set clock hands
+	var handHour = document.getElementById('handHour');
+	var handMinute = document.getElementById('handMinute');
+	
+	// Subtract 90 degrees to account for the start position
+	var rotateHour = ( parseInt(hours) * 30 ) - 90;
+	var rotateMinute = ( parseInt(minutes) * 6 ) - 90;
+
+	handHour.style.webkitTransform = 'rotate('+rotateHour+'deg)';
+	handHour.style.mozTransform    = 'rotate('+rotateHour+'deg)';
+	handHour.style.msTransform     = 'rotate('+rotateHour+'deg)';
+	handHour.style.oTransform      = 'rotate('+rotateHour+'deg)';
+	handHour.style.transform       = 'rotate('+rotateHour+'deg)';
+
+	handMinute.style.webkitTransform = 'rotate('+rotateMinute+'deg)';
+	handMinute.style.mozTransform    = 'rotate('+rotateMinute+'deg)';
+	handMinute.style.msTransform     = 'rotate('+rotateMinute+'deg)';
+	handMinute.style.oTransform      = 'rotate('+rotateMinute+'deg)';
+	handMinute.style.transform       = 'rotate('+rotateMinute+'deg)';
+
+	// Format minutes with leading zero
+	if ( minutes < 10 ) {
+		minutes = "0" + minutes;
+	}
+
+	// Set clock local time
+	var time = hours + ":" + minutes;
+	clock.innerHTML = time;
+}
+
 $(function(){
-	new Application(); 
+	new Application();
+	localTime();
+	setInterval( function(){ localTime() }, 10000 );
 });
