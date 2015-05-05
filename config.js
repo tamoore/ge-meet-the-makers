6eae3adc07d0e9ed30bbf8a432a8543ee368fc0c
Todd Moore <todd.moore@theguardian.com>
2015-05-05 19:25:06 +1000

    adding the move timeline video

--- config.js
+++ config.js
@@ -21,6 +21,7 @@ System.config({
     "backbone.wreqr": "github:marionettejs/backbone.wreqr@1.3.2",
     "components/handlebars.js": "github:components/handlebars.js@3.0.2",
     "core-js": "npm:core-js@0.9.5",
+    "easeljs": "github:CreateJS/EaselJS@0.8.0",
     "hammerjs": "npm:hammerjs@2.0.4",
     "handlebars": "github:components/handlebars.js@3.0.2",
     "jquery": "github:components/jquery@2.1.3",
@@ -30,31 +31,105 @@ System.config({
     "marionettejs/backbone.babysitter": "github:marionettejs/backbone.babysitter@0.1.6",
     "marionettejs/backbone.marionette": "github:marionettejs/backbone.marionette@2.4.1",
     "marionettejs/backbone.wreqr": "github:marionettejs/backbone.wreqr@1.3.2",
+    "node-easel": "npm:node-easel@0.1.2",
+    "preload": "github:CreateJS/PreloadJS@0.6.0",
     "txt": "github:systemjs/plugin-text@0.0.2",
     "underscore": "npm:underscore@1.8.3",
+    "github:jspm/nodelibs-buffer@0.1.0": {
+      "buffer": "npm:buffer@3.2.2"
+    },
+    "github:jspm/nodelibs-events@0.1.0": {
+      "events-browserify": "npm:events-browserify@0.0.1"
+    },
+    "github:jspm/nodelibs-os@0.1.0": {
+      "os-browserify": "npm:os-browserify@0.1.2"
+    },
+    "github:jspm/nodelibs-path@0.1.0": {
+      "path-browserify": "npm:path-browserify@0.0.0"
+    },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
-    "github:n-fuse/plugin-ember-hbs@1.11.1": {
-      "ember-template-compiler": "github:n-fuse/ember-template-compiler@1.11.3"
+    "github:jspm/nodelibs-stream@0.1.0": {
+      "stream-browserify": "npm:stream-browserify@1.0.0"
+    },
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
     },
     "npm:backbone@1.1.2": {
       "process": "github:jspm/nodelibs-process@0.1.1",
       "underscore": "npm:underscore@1.8.3"
     },
-    "npm:core-js@0.8.4": {
-      "process": "github:jspm/nodelibs-process@0.1.1"
+    "npm:buffer@3.2.2": {
+      "base64-js": "npm:base64-js@0.0.8",
+      "ieee754": "npm:ieee754@1.1.5",
+      "is-array": "npm:is-array@1.0.1"
     },
-    "npm:core-js@0.9.3": {
-      "process": "github:jspm/nodelibs-process@0.1.1"
+    "npm:canvas@1.2.2": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "nan": "npm:nan@1.5.3",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "stream": "github:jspm/nodelibs-stream@0.1.0",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
     "npm:core-js@0.9.5": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:core-util-is@1.0.1": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0"
+    },
+    "npm:events-browserify@0.0.1": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:hammerjs@2.0.4": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
+    "npm:inherits@2.0.1": {
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
     "npm:lodash@3.7.0": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:nan@1.5.3": {
+      "path": "github:jspm/nodelibs-path@0.1.0"
+    },
+    "npm:node-easel@0.1.2": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "canvas": "npm:canvas@1.2.2",
+      "child_process": "github:jspm/nodelibs-child_process@0.1.0",
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "os": "github:jspm/nodelibs-os@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:os-browserify@0.1.2": {
+      "os": "github:jspm/nodelibs-os@0.1.0"
+    },
+    "npm:path-browserify@0.0.0": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:readable-stream@1.1.13": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "core-util-is": "npm:core-util-is@1.0.1",
+      "events": "github:jspm/nodelibs-events@0.1.0",
+      "inherits": "npm:inherits@2.0.1",
+      "isarray": "npm:isarray@0.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "stream": "github:jspm/nodelibs-stream@0.1.0",
+      "stream-browserify": "npm:stream-browserify@1.0.0",
+      "string_decoder": "npm:string_decoder@0.10.31",
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:stream-browserify@1.0.0": {
+      "events": "github:jspm/nodelibs-events@0.1.0",
+      "inherits": "npm:inherits@2.0.1",
+      "readable-stream": "npm:readable-stream@1.1.13"
+    },
+    "npm:string_decoder@0.10.31": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0"
+    },
+    "npm:util@0.10.3": {
+      "inherits": "npm:inherits@2.0.1",
       "process": "github:jspm/nodelibs-process@0.1.1"
     }
   }
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,25 +1,96 @@
 import $ from 'jquery';
 import Hammer from 'hammerjs';
 import _ from 'lodash';
+import preloadjs from 'preload';
+import easeljs from 'easeljs';
+
+//===============================================
+// Constant VARIABLES
+//===============================================
+const MAKER_AMBIENT_PREFIX = "maker";
+const MAKER_AMIENT_COUNT = 12;
+const MAKER_COUNT = 1; // placeholder to deal with makers at a later stage
+const MAKER_AMBIENT_TYPES = ['jpg', 'mp4'];
+const CDN_HOST = "cdn.labs.theguardian.com";
+const CDN_BUCKET = "2015/meet-the-makers/images/"
+const CDN_PROTOCOL = "http";
 
 class Application {
 	constructor(){
 		this.now = new Date();
 		this.clock = document.getElementById('localTime');
 		this.currentOffset = 0;
+		this.assets = [];
+		this.tempProgress = document.getElementById("tempProgress");
 
 		// Basic timeline attachment
 		this.timeline = document.getElementById('timelineWrapper');
 		this.timelineList = document.getElementById('timelineList');
-
 
 		this.timelineMC = new Hammer(this.timeline);
 		this.timelineMC.get('pan').set({ direction: Hammer.DIRECTION_HORIZONTAL });
 		this.timelineMC.on('panleft panright', _.bind(this.handlePanOfTimeline, this));
 		this.timelineMC.on('panend', _.bind(this.handlePanEnd, this));
 
-		this.createLocalTimeCounter();
+		this.preload = new createjs.LoadQueue();
+		this.preload.on("progress", this.preloadProgress, this);
+		this.preload.on("complete", this.completedProgress, this);
 
+		// Create the stage
+		this.stage = new createjs.Stage("ambientvideos");
+
+
+		console.log(this.stage.getBounds());
+
+		this._generateLoaderAssets();
+		this.fetchAssets();
+
+		this.createLocalTimeCounter();
+	}
+
+	_generateLoaderAssets(){
+		var makercount = 1;
+		var assetcount = 1;
+
+		let progressTypes = (makerprefix, assetprefix, makercount, assetcount)=>{
+			MAKER_AMBIENT_TYPES.forEach((type)=>{
+				this.assets.push({ "id": "maker"+makerprefix+makercount+"_"+assetprefix+assetcount+"_"+type,  "type": type, "filename" : CDN_PROTOCOL + "://" + CDN_HOST + "/" + CDN_BUCKET + MAKER_AMBIENT_PREFIX+makerprefix+makercount+'_'+assetprefix+assetcount+'.'+type });
+			});
+		}
+		while(makercount <= MAKER_COUNT){
+			while(assetcount <= MAKER_AMIENT_COUNT){
+				if(assetcount < 10){
+					progressTypes('0', '0', makercount, assetcount);
+
+				}else{
+					progressTypes('0','', makercount, assetcount);
+				}
+				assetcount++;
+			}
+			makercount++;
+		}
+		console.log(this.assets);
+	}
+
+	preloadProgress(event){
+		this.tempProgress.textContent = Math.ceil(event.progress*100)+"%";
+	}
+
+	completedProgress(event){
+		var image = new createjs.Bitmap(this.preload.getResult("maker01_01_jpg"));
+		this.stage.addChild(image);
+		this.stage.update();
+	}
+
+	fetchAssets(){
+		return new Promise((resolved, rejected)=>{
+			let jpgs = this.assets.filter((item)=>{
+				return item.type == "jpg";
+			});
+			jpgs.forEach((item)=>{
+				this.preload.loadFile({id: item.id, src: item.filename, crossOrigin: true });
+			});
+		});
 	}
 
 	handlePanEnd(event){
@@ -28,10 +99,33 @@ class Application {
 
 	handlePanOfTimeline(event){
 		let offset = event.deltaX;
+		var intervalDifference = 274;
+		var index;
 		this.currentOffset = this.currentOffset + (offset/5);
-		this.timelineList.style.transform = "translateX("+this.currentOffset+"px)";
+
+		if(this.currentOffset > -3280 && this.currentOffset < 1){
+			this.timelineList.style.webkitTransform = "translateX("+this.currentOffset+"px)";
+			this.timelineList.style.MozTransform = "translateX("+this.currentOffset+"px)";
+			this.timelineList.style.msTransform = "translateX("+this.currentOffset+"px)";
+			this.timelineList.style.OTransform = "translateX("+this.currentOffset+"px)";
+			this.timelineList.style.transform = "translateX("+this.currentOffset+"px)";
+		}
+		if(this.currentOffset > 1){
+			this.currentOffset = 0;
+		}
+		if(this.currentOffset < -3281){
+			this.currentOffset = -3280
+		}
+		var index = Math.ceil(Math.abs(this.currentOffset)/intervalDifference);
+		this.changeCurrentStatic(index < 10 ? '0'+index : index);
 
 	}
+	changeCurrentStatic(index){
+		var image = new createjs.Bitmap(this.preload.getResult("maker01_"+index+"_jpg"));
+		this.stage.addChild(image);
+		this.stage.update();
+	}
+
 
 	createLocalTimeCounter(){
 
@@ -77,7 +171,6 @@ class Application {
 	}
 
 }
-
 
 $(function(){
 	new Application();
