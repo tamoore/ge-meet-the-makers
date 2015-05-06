335c2a8a9818fa4db605e807190af3f7f114a490
James Robinson <james@imaginary.com.au>
2015-05-06 13:53:06 +1000

    Circle Timeline changes - circle now resizes to fit available height - added circle labels - added instructions

--- src/lib/circle.js
+++ src/lib/circle.js
@@ -4,6 +4,8 @@ $(function(){
 
 	$('.marker-data a').on('mouseover', function(e){
 
+		$('.timeline-instructions').addClass('hidden');
+
 		// Get values
 		var marker = $(this);
 		var previewSrc = marker.data('preview');
@@ -19,15 +21,16 @@ $(function(){
 		var previewBlurb = $('#previewBlurb');
 
 		// Get circle dimensions
-		var circle = $('#timelineList');
+		var circleWrap = $('#timelineCircleWrapper');
+		var circleLeft = circleWrap.offset().left;
+		var circleTop = circleWrap.offset().top;
 
 		// Get marker positions
-		var previewImageLeft =  marker.offset().left - ( previewImage.width() / 2 );
-		var previewImageTop =  marker.offset().top - ( previewImage.height() / 2 );
+		var previewImageLeft =  marker.offset().left - circleLeft - ( previewImage.width() / 2 ) + (marker.width() /2);
+		var previewImageTop =  marker.offset().top - circleTop -  ( previewImage.height() / 2 ) + (marker.width() /2);
 
-		previewImage
-			.attr('src', previewSrc)
-			.css({'left':  previewImageLeft, 'top': previewImageTop}).addClass('preview-display');
+		$('img', previewImage).attr('src', previewSrc);
+		previewImage.css({'left':  previewImageLeft, 'top': previewImageTop}).addClass('preview-display');
 
 		previewType.html(previewDataType);
 		previewHeading.html(previewTitle);
@@ -40,6 +43,7 @@ $(function(){
 
 	}).on('mouseout', function(e){
 		$('.preview-object').removeClass('preview-display');
+		$('.timeline-instructions').removeClass('hidden');
 	});
 
 });
